f8ff6320b43755b6d58283a01d27121fdfe44072
Jannik Birk <birk.jannik@gmail.com>
2021-01-09 16:35:39 +0100

    show date and time on matches that didn't start yet

--- buli.py
+++ buli.py
@@ -2,19 +2,26 @@
 
 import requests
 import json
+from datetime import datetime
 
 def print_match(match, i):
     t1 = match["Team1"]["ShortName"]
     t2 = match["Team2"]["ShortName"]
     vs = "  vs "
 
+    print(f"{i}) ", end="")
+
+    # If the game already started display the score instead of vs
+    # If the game is in the future show the date after the teams
     result = match["MatchResults"]
     if len(result) > 0:
         t1g = result[0]["PointsTeam1"]
         t2g = result[0]["PointsTeam2"]
         print(f"{t1:>15} [{t1g}:{t2g}] {t2:15}", end="")
     else:
+        dt = datetime.fromisoformat(match["MatchDateTime"])
         print(f"{t1:>15}   vs  {t2:15}", end="")
+        print(dt.strftime("%a %H:%M"), end="")
 
     print()
 
