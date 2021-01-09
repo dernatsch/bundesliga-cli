cb1ede0a0d6fa0bd6f508b1b7531bda46f8c8731
Jannik Birk <birk.jannik@gmail.com>
2021-01-09 17:50:11 +0100

    add goals to output

--- buli.py
+++ buli.py
@@ -1,10 +1,21 @@
 #! /usr/bin/env python3
 
+import argparse
+
 import requests
 import json
 from datetime import datetime
 
-def print_match(match, i):
+def print_goals(goals):
+    for goal in goals:
+        t = goal["MatchMinute"]
+        scorer = goal["GoalGetterName"]
+
+        print(f"                    {t}' {scorer}")
+
+    print()
+
+def print_match(match, i, printgoals=False):
     t1 = match["Team1"]["ShortName"]
     t2 = match["Team2"]["ShortName"]
     vs = "  vs "
@@ -25,12 +36,23 @@ def print_match(match, i):
 
     print()
 
+    if printgoals:
+        print_goals(match["Goals"])
+
 def main():
+    parser = argparse.ArgumentParser(
+        description="Show Bundesliga scores on the command line."
+    )
+
+    parser.add_argument("--goals", dest="goals", action="store_true",
+            help="print the goals of each game")
+    args = parser.parse_args()
+
     r = requests.get("https://openligadb.de/api/getmatchdata/bl1")
     data = r.json()
 
     for i, match in enumerate(data):
-        print_match(match, i)
+        print_match(match, i, printgoals=args.goals)
 
 
 if __name__=="__main__":
